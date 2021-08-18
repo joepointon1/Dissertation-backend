94aaa670d825c5ae89f44ec4185dd6935d7af268
C1623024 <pointonJ@cardiff.ac.uk>
2021-08-18 11:07:12 +0100

    second commit

--- client/src/Screens/Screen2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native-paper'
-const Screen2 = ({navigation, route}) => {
-    return <Text>text:{route.params.paramA}</Text>
-  }
-
-export default Screen2;
--- client/src/Styles/button.js
+++
@@ -1,5 +0,0 @@
-export const buttonStyle = {
-    width: 200, 
-    paddingTop: 10,
-    paddingBottom: 10
-}
