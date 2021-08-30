173e5ecd3e902ab39a8e6e37be9fb4e8850a1fae
C1623024 <pointonJ@cardiff.ac.uk>
2021-08-30 20:42:21 +0100

    commit

--- models/patients.js
+++ models/patients.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
-const PatientSchema = new mongoose.Schema({
+const PatientSchema = new mongoose.Schema(
+  {
     therapistId: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
+    },
+    patientId: {
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    firstName:{
-        type:String,
-        require: true
+    firstName: {
+      type: String,
+      require: true,
     },
-    lastName:{
-        type:String,
-        require:true
-    }
-},
-    {
-        timestamps:true
-    }
-)
-PatientSchema.index({ "email" : "text"})
+    lastName: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+PatientSchema.index({ email: "text" });
 
-const Patient = mongoose.model('Patient', PatientSchema);
+const Patient = mongoose.model("Patient", PatientSchema);
 
 export default Patient;
