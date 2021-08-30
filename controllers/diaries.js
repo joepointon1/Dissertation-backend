a1f392fc6f6bf32a1fb8050942052cc840c68830
C1623024 <pointonJ@cardiff.ac.uk>
2021-08-30 21:24:38 +0100

    get diary entires in helper function

--- controllers/diaries.js
+++ controllers/diaries.js
@@ -2,132 +2,152 @@ import User from "../models/user.js";
 import indexOfAttr from "../helpers/indexOfAttr.js";
 import Diary from "../models/diary.js";
 import Patient from "../models/patients.js";
-const createDiaryEntry = (req, res) =>{
-    const userId = req.userId;
-    
-    if (!req.body.title){
-        res.status(400).send({message: "Title must not be blank"});
-        return;
-    }
-    const diary = new Diary({user:userId,...req.body})
-    diary.save((err, entry) =>{
-        if(err) return res.status(500).send({message: err.message})
-        res.send({message:"Diary logged"});
-    })
+const createDiaryEntry = (req, res) => {
+  const userId = req.userId;
 
+  if (!req.body.title) {
+    res.status(400).send({ message: "Title must not be blank" });
+    return;
+  }
+  const diary = new Diary({ user: userId, ...req.body });
+  diary.save((err, entry) => {
+    if (err) return res.status(500).send({ message: err.message });
+    res.send({ message: "Diary logged" });
+  });
 };
 
 const updateDiaryEntry = (req, res) => {
-    if (!req.body){
-        return res.status(400).send({message: "body cannot be empty"})
+  if (!req.body) {
+    return res.status(400).send({ message: "body cannot be empty" });
+  }
+  const userId = req.userId;
+  const diaryId = req.params.id;
+  console.log(req.body.distortions);
+  Diary.updateOne(
+    { user: userId, _id: diaryId },
+    {
+      title: req.body.title,
+      event: req.body.event,
+      thoughts: req.body.thoughts,
+      beliefs: req.body.beliefs,
+      anxiety: req.body.anxiety,
+      distortions: req.body.distortions,
+      altInterpretation: req.body.altInterpretation,
+      private: req.body.private,
     }
-    const userId = req.userId;
-    const diaryId = req.params.id;
-    console.log(req.body.distortions)
-    Diary.updateOne(
-        {"user":userId, "_id": diaryId },
-                {"title": req.body.title, 
-                "event": req.body.event, 
-                "thoughts": req.body.thoughts, 
-                "beliefs":req.body.beliefs, 
-                "anxiety": req.body.anxiety, 
-                "distortions": req.body.distortions, 
-                "altInterpretation": req.body.altInterpretation, 
-                "private":req.body.private}
-    ).then(data =>{
-        return res.send(data)
-    }).catch(err =>{
-        return res.status(404).send({message:err});
+  )
+    .then((data) => {
+      return res.send(data);
     })
-    
-}
+    .catch((err) => {
+      return res.status(404).send({ message: err });
+    });
+};
 
 const deleteDiaryEntry = (req, res) => {
-    const diaryId = req.params.id;
-    const userId = req.userId;
-    
-    //find user, find diary entey, delete it
-    Diary.deleteOne({user:userId, _id: diaryId})
-        .then(data =>{
-            return res.send(data)
-        }).catch(err =>{
-            return res.status(404).send({message:err.message});
-        })
-   
-}
+  const diaryId = req.params.id;
+  const userId = req.userId;
+
+  //find user, find diary entey, delete it
+  Diary.deleteOne({ user: userId, _id: diaryId })
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((err) => {
+      return res.status(404).send({ message: err.message });
+    });
+};
 
 const getDiaryEntry = (req, res) => {
-    const userId = req.userId;
-    const diaryId = req.params.id;
+  const userId = req.userId;
+  const diaryId = req.params.id;
 
-    Diary.find({user:userId,_id:diaryId})
-        .then(data => {
-            if(data.length == 0) return res.status(404).send({message:`No entries found with id ${diaryId}`})
-            return res.send(data);
-        })
-        .catch(err =>{
-            res.status(500).send({message:`Error while trying to retrieve diary entry with id ${diaryId}`})
-        })
-    
-}
+  Diary.find({ user: userId, _id: diaryId })
+    .then((data) => {
+      if (data.length == 0)
+        return res
+          .status(404)
+          .send({ message: `No entries found with id ${diaryId}` });
+      return res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error while trying to retrieve diary entry with id ${diaryId}`,
+      });
+    });
+};
 
 const getAllDiaryEntries = (req, res) => {
-    const userId = req.userId; 
-    
-    Diary.find({user:userId}, {title:1, event:1, date:1, updatedAt:1})
-        .then(data => {
-            if(data.length == 0) return res.status(404).send({message:`No entries found for user id ${userId}`})
-            return res.send(data);
-        })
-        .catch(err =>{
-            res.status(500).send({message:`Error while trying to retrieve diary entries for user id ${userId}`})
-        })
-    
-}
+  getDiaryEntries(req.userId, res)
+};
 
-const getPatientsEntries = (req,res) => {
-    isPatientInTherapistList(req.userId)
-}
+const getPatientsEntries = (req, res) => {
+  if (isPatientInTherapistList(req.userId, req.body.patientId)) {
+    getDiaryEntries(req.userId, res)
+  }else{
+      return res.status(404).send({messsage:"User not in therapist list"})
+  }
+};
 
 const searchDiaryEntries = async (req, res) => {
-    const userId = req.userId;
-    const searchString = req.body.search;
+  const userId = req.userId;
+  const searchString = req.body.search;
 
-    Diary.find({user:userId}).find({$text: {$search: searchString, $caseSensitive:false, $language:"en"}})
-        .then(data =>{
-            if (data.length == 0) return res.status(204).send({message:"No results found for that search term"})
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send(err)
-        })
-   
-}
+  Diary.find({ user: userId })
+    .find({
+      $text: { $search: searchString, $caseSensitive: false, $language: "en" },
+    })
+    .then((data) => {
+      if (data.length == 0)
+        return res
+          .status(204)
+          .send({ message: "No results found for that search term" });
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+};
 
 export default {
-    createDiaryEntry,
-    updateDiaryEntry,
-    deleteDiaryEntry,
-    getDiaryEntry,
-    getAllDiaryEntries,
-    searchDiaryEntries,
-    getPatientsEntries
+  createDiaryEntry,
+  updateDiaryEntry,
+  deleteDiaryEntry,
+  getDiaryEntry,
+  getAllDiaryEntries,
+  searchDiaryEntries,
+  getPatientsEntries,
+};
+
+function isPatientInTherapistList(id, patientId) {
+  Patient.find({ therapistId: id })
+    .then((data) => {
+      if (data.length == 0) return false;
+      const patient = data.find((p) => p._id == patientId);
+      if (patient) {
+        return true
+      } else {
+        return false;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return;
+    });
 }
 
-function isPatientInTherapistList(id){
-    const userId = req.userId;
-
-    Patient.find({therapistId: id})
-        .then(data =>{
-            if (data.length == 0) return false
-            const patient = data.find(p => p.therapistId == userId);
-            if (patient){
-                return true;
-            }else{
-                return false;
-            }
-        }).catch(err =>{
-            console.log(err);
-            return
-        })
+function getEntries(userId, res){
+    Diary.find({ user: userId }, { title: 1, event: 1, date: 1, updatedAt: 1 })
+    .then((data) => {
+      if (data.length == 0)
+        return res
+          .status(404)
+          .send({ message: `No entries found for user id ${userId}` });
+      return res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error while trying to retrieve diary entries for user id ${userId}`,
+      });
+  });
 }
